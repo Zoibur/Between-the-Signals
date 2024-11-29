f548c08da18367878b7d7a7809b513843e4a2843
Edvin Hansson <60141840+edvinhansson@users.noreply.github.com>
2024-11-29 19:24:49 +0100

    Add light flickering event, fix bugs with UI

---
+++ Assets/LightController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public AnimationCurve lightIntensity;
+    
+    private bool isFlickering = false;
+    public Light light1;
+    public Light light2;
+
+    private IEnumerator Flicker()
+    {
+        float originalIntensity1 = light1.intensity;
+        float originalIntensity2 = light2.intensity;
+        isFlickering = true;
+
+        float progress = 0.0f;
+        while (progress < 1.0f) {
+            light1.intensity = lightIntensity.Evaluate(progress);
+            light2.intensity = lightIntensity.Evaluate(progress) * 8;
+            progress += Time.deltaTime;
+            yield return null;
+        }
+        
+        isFlickering = false;
+        light1.intensity = originalIntensity1;
+        light2.intensity = originalIntensity2;
+    }
+    
+    private void OnEvent(EventManager.EventID eventID)
+    {
+        if (eventID == EventManager.EventID.LightFlicker && !isFlickering) {
+            StartCoroutine(Flicker());
+        }
+    }
+
+    private void OnEnable()
+    {
+        EventManager.OnEventRaised += OnEvent;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnEventRaised -= OnEvent;
+    }
+}
